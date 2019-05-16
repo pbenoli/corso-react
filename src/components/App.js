45e09046d77bda95cbef1e5ea5ff09c870415b6a
pbenoli <paolo.benoli@goiss.it>
2019-05-16 17:29:05 +0200

    funzionamento base

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,64 @@ import React, { Component } from 'react';
 import Button from './Button';
 import Display from './Display';
 
+import { PLUS, MINUS, MULTI, DIVIDE, EQUAL, DOT } from '../config/const';
 import '../styles/App.scss';
-
+import { includes } from 'lodash';
 class App extends Component {
   constructor() {
     super();
+    //memorizza le cifre e la virgola
+    this.digits = [];
 
-    this.state = {};
+    //memorizza primo e secondo operando
+    this.operators = [];
+    this.state = {
+      display: '',
+      operation: '',
+    };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
+  doComputation() {
+    this.operators.push(parseFloat(this.state.display));
+    console.log('switch this.state.operation=', this.state.operation);
+    let result = '';
+    switch (this.state.operation) {
+      case PLUS:
+        result = this.operators[0] + this.operators[1];
+      case MINUS:
+        result = this.operators[0] - this.operators[1];
+      case MULTI:
+        result = this.operators[0] * this.operators[1];
+      case DIVIDE:
+        result = this.operators[0] / this.operators[1];
+      default:
+        break;
+    }
+    this.setState({ display: parseFloat(result), operation: '' });
+    this.operators[0] = this.operators[1];
+    this.operators.shift();
+    console.log('shift', this.operators);
+  }
+
+  handleClick(label) {
     // Gestore del click
+    console.log('label;', label);
+    switch (label) {
+      case PLUS:
+      case MULTI:
+      case MINUS:
+      case DIVIDE:
+        this.setOperation(label);
+        break;
+      case EQUAL:
+        this.doComputation();
+        break;
+      case DOT:
+      default:
+        this.updateDigits(label);
+    }
   }
 
   render() {
@@ -22,26 +67,45 @@ class App extends Component {
       <div>
         <h1>Calcolatrice REACT</h1>
         <div id="calculator">
-          <Display />
-          <Button label="7" click={() => console.log('Click del Button')} />
-          <Button label="8" click={() => console.log('Click del Button')} />
-          <Button label="9" click={() => console.log('Click del Button')} />
-          <Button label=":" click={() => console.log('Click del Button')} orange />
-          <Button label="4" click={() => console.log('Click del Button')} />
-          <Button label="5" click={() => console.log('Click del Button')} />
-          <Button label="6" click={() => console.log('Click del Button')} />
-          <Button label="X" click={() => console.log('Click del Button')} orange />
-          <Button label="1" click={() => console.log('Click del Button')} />
-          <Button label="2" click={() => console.log('Click del Button')} />
-          <Button label="3" click={() => console.log('Click del Button')} />
-          <Button label="-" click={() => console.log('Click del Button')} orange />
-          <Button label="0" click={() => console.log('Click del Button')} />
-          <Button label="," click={() => console.log('Click del Button')} />
-          <Button label="=" click={() => console.log('Click del Button')} orange />
-          <Button label="+" click={() => console.log('Click del Button')} orange />
+          <Display value={this.state.display} />
+          <Button label="7" click={this.handleClick} />
+          <Button label="8" click={this.handleClick} />
+          <Button label="9" click={this.handleClick} />
+          <Button label={DIVIDE} click={this.handleClick} orange />
+          <Button label="4" click={this.handleClick} />
+          <Button label="5" click={this.handleClick} />
+          <Button label="6" click={this.handleClick} />
+          <Button label={MULTI} click={this.handleClick} orange />
+          <Button label="1" click={this.handleClick} />
+          <Button label="2" click={this.handleClick} />
+          <Button label="3" click={this.handleClick} />
+          <Button label={MINUS} click={this.handleClick} orange />
+          <Button label="0" click={this.handleClick} />
+          <Button label={DOT} click={this.handleClick} />
+          <Button label={EQUAL} click={this.handleClick} orange />
+          <Button label={PLUS} click={this.handleClick} orange />
         </div>
       </div>
     );
+  }
+
+  setOperation(operation) {
+    console.log('operation:', operation);
+    this.setState({ operation }); //se variabile ha lo stesso nome della chiave,
+    // non serve indicare la chiave
+    // ns // const firstNumber = this.state.display;
+    // aggiungo primo operando
+    this.operators.push(parseFloat(this.state.display));
+    // svoda digits
+    this.digits = [];
+    console.log('this.operators=', this.operators, ' this.digits=', this.digits);
+  }
+
+  updateDigits(digit) {
+    console.log('digit:', digit);
+    if (digit != DOT || (digit === DOT && !includes(this.digits, digit)));
+    this.digits.push(digit);
+    this.setState({ display: this.digits.join('') });
   }
 }
 
--- src/config/const.js
+++ src/config/const.js
@@ -2,6 +2,7 @@ export const PLUS = '+';
 export const MINUS = '-';
 export const MULTI = '*';
 export const DIVIDE = '/';
-
+export const EQUAL = '=';
+export const DOT = '.';
 export const SET = 'set';
 export const RESET = 'reset';
